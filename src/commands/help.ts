b73cb0781b6f0e2394e4e20fd58f567b578d88e0
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-20 00:00:54 +0530

    Pushing code

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,7 +1,23 @@
-import {Message} from 'discord.js';
+import {config} from 'dotenv';
+import {Message, MessageEmbed} from 'discord.js';
 
-const run = (message: Message) => {
-    //
+config();
+const prefix = process.env.BOT_PREFIX || '->';
+
+const run = async (message: Message) => {
+    const embed = new MessageEmbed({
+        title: 'Help wanted nigga?',
+        description: 'Here you go. Now suck my dick!',
+        fields: [
+            {
+                name: prefix + 'delete',
+                value: 'Pass a link and it will go kaboom',
+                inline: true
+            }
+        ]
+    });
+
+    await message.author.send(embed);
 }
 
 export default {
